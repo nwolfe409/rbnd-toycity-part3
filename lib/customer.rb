148d4749836efcbf67b9e3c6dbc592d1c6ac40ce
Nathan Wolfe <nwolfe409@gmail.com>
2016-02-27 19:00:53 +0000

    Working base code for Customers section of app.rb

---
+++ lib/customer.rb
@@ -0,0 +1,35 @@
+# lib/customer.rb
+
+class Customer
+  attr_reader :name
+
+  @@customers = []
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+  
+  
+  
+  
+  
+  def self.find_by_name(name) #returns customer by name
+   @@customers.find { |customer| customer.name == name }
+  end
+
+  private
+
+  def add_to_customers
+    @@customers.each do |customer|
+      raise DuplicateCustomerError, "#{@name} already exists." if customer.name == @name
+    end
+    @@customers << self
+  end
+  
+  
+end
