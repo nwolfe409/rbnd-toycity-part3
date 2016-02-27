fcc6077b385abe5302eb5dd43fd0f432785566e0
Nathan Wolfe <nwolfe409@gmail.com>
2016-02-27 04:31:52 +0000

    Updated produt.rb to prevent adding duplicate items

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,25 @@
 # lib/product.rb
 
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    @@products.each do |product|
+      raise DuplicateProductError, "#{@title} already exists." if product.title == @title
+    end
+    @@products << self
   end
 end
